a03965d0f24e7d1ef181201923f4de03541f8d38
Rae <raemmacias@gmail.com>
2019-04-23 19:34:22 -0600

    Initial commit

---
+++ app/src/main/java/com/androidatelier/fillmyglass/CustomViewActivity.kt
@@ -0,0 +1,12 @@
+package com.androidatelier.fillmyglass
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomViewActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_custom_view)
+  }
+}
---
+++ app/src/main/java/com/androidatelier/fillmyglass/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.androidatelier.fillmyglass
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    customViewButton.setOnClickListener {
+      val startIntent = Intent(this, CustomViewActivity::class.java)
+      startActivity(startIntent)
+    }
+  }
+}
