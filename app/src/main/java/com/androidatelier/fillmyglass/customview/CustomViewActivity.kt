ac767ca130b91e86513a1d6223f8e59b8dcd58a4
Rae <raemacias@users.noreply.github.com>
2019-04-23 19:47:25 -0600

    moved activities into subdirectories

--- app/src/main/java/com/androidatelier/fillmyglass/customview/CustomViewActivity.kt
+++ app/src/main/java/com/androidatelier/fillmyglass/customview/CustomViewActivity.kt
@@ -1,7 +1,8 @@
-package com.androidatelier.fillmyglass
+package com.androidatelier.fillmyglass.customview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.androidatelier.fillmyglass.R
 
 class CustomViewActivity : AppCompatActivity() {
 
--- app/src/main/java/com/androidatelier/fillmyglass/main/MainActivity.kt
+++ app/src/main/java/com/androidatelier/fillmyglass/main/MainActivity.kt
@@ -1,8 +1,10 @@
-package com.androidatelier.fillmyglass
+package com.androidatelier.fillmyglass.main
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.androidatelier.fillmyglass.customview.CustomViewActivity
+import com.androidatelier.fillmyglass.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
