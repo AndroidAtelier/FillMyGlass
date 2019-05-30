b28f82638d7c68d684795824a60babfce4952bb4
Baghaii <baghaii@gmail.com>
2019-05-29 19:12:09 -0600

    Fill the glass

--- app/src/main/java/com/androidatelier/fillmyglass/customview/GlassView.kt
+++ app/src/main/java/com/androidatelier/fillmyglass/customview/GlassView.kt
@@ -3,6 +3,8 @@ package com.androidatelier.fillmyglass.customview
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -10,7 +12,28 @@ class GlassView @JvmOverloads constructor(
   context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+  private val glassPath: Path by lazy {
+    Path().apply {
+      moveTo(0f,0f)
+      lineTo(0f, height.toFloat())
+      lineTo(width.toFloat(), height.toFloat())
+      lineTo(width.toFloat(), 0f)
+    }
+  }
+
+  private val glassPaint = Paint().apply {
+    color = Color.BLACK
+    style = Paint.Style.STROKE
+    strokeWidth = 8.0f
+  }
+
+  private val liquidPaint = Paint().apply {
+    color = Color.CYAN
+    style = Paint.Style.FILL
+  }
+
   override fun onDraw(canvas: Canvas?) {
-    canvas?.drawColor(Color.BLUE)
+    canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(), liquidPaint)
+    canvas?.drawPath(glassPath, glassPaint)
   }
 }
